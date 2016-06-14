eac6e6f151e4ee246ef26c6911a464df0294ddc5
Chris Meyer <meyer.cm@gmail.com>
2016-06-14 08:51:54 -0400

    typo in mix.exs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Flub.Mixfile do
   def project do
     [
       app: :flub,
-      version: @version
+      version: @version,
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
